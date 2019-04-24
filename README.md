7576f94cd7ee669546ee05c15a7b6611ab30864d
Vojislav Grujic <vojislav.grujic@gmail.com>
2019-04-24 20:35:47 +0200

    Initial webpack app setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Transactions
+
+Install dependencies:
+
+`yarn` or `npm install`
+
+Run the development mode:
+
+`yarn start` or `npm start`
+
+Run the build process:
+
+`yarn build` or `npm run build`
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: 'src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            // {
+            //     test: /\.(jpg|png)$/,
+            //     use: [
+            //         {
+            //             loader: 'file-loader',
+            //             options: {
+            //                 name: '[name].[ext]',
+            //                 outputPath: 'img/',
+            //                 publicPath: 'img/'
+            //             }
+            //         }
+            //     ]
+            // }
+        ]
+    }
+};
