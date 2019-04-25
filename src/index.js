3c7813080006fd56cf4a77bb2145462a21f7bde8
Vojislav Grujic <vojislav.grujic@gmail.com>
2019-04-26 00:49:46 +0200

    Replace icons

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 import './assets/logo.jpg';
 import './assets/background.jpg';
 import './assets/icons/transaction.svg';
-import './assets/icons/arrows.png';
+import './assets/icons/arrows.svg';
+import './assets/icons/briefcase.svg';
 
 import './css/style.scss';
 import './js/main';
